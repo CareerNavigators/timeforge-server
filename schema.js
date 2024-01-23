5571c8f092af5f7a504602eefd1a50767c31bc7a
434huzaifa <saadhuzaifa2497@gmail.com>
2024-01-23 11:58:45 +0600

    user schema added

--- schema.js
+++ schema.js
@@ -1,2 +1,34 @@
 const mongo =require("mongoose")
 const humanizeErrors = require('mongoose-error-humanizer')
+
+const userSchema=new mongo.Schema({
+    name:{
+        type:String,
+        default:null,
+        
+    },
+    email:{
+        type:String,
+        default:null,
+        lowercase:true,
+        unique:true,
+    },
+    bio:{
+        type:String,
+        default:null,
+    },
+    country:{
+        type:String,
+        default:null,
+    },
+    timeZone:{
+        type:String,
+        default:null
+    },
+    image:{
+        type:String,
+        default:null,
+    }
+},{
+    timestamps:true,
+})
